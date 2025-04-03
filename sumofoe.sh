03af4c5ceee0d42b54b3cfb45f9dc6c63ad82e50
Seena <167744692+seenazn@users.noreply.github.com>
2025-04-03 14:30:09 +0530

    Add files via upload

---
+++ sumofoe.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+echo "Enter a number"
+read num
+rev=0
+even=0
+odd=0
+while [ $num -gt 0 ]
+do
+tmp=$(($num%10))
+if(($tmp%2==0))
+then
+even=$(($even+$tmp))
+else
+odd=$(($odd+$tmp))
+fi
+rev=$(($rev*10+$tmp))
+num=$(($num/10))
+done
+echo "the sum of even number $even"
+echo "the sum of odd number $odd"
+echo "the reversed number is $rev"
